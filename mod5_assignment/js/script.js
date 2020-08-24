19dccb9e24e576f2e2d16aa9ac16a7bd3df69a70
SSPlusUltra <52967609+SSPlusUltra@users.noreply.github.com>
2020-08-24 18:37:05 +0530

    mod5_assignment

--- mod5_assignment/js/script.js
+++ mod5_assignment/js/script.js
@@ -97,11 +97,10 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-     var modifiedChosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-      var homeHtmlToInsertIntoMainPage = homeHtml;
-      homeHtmlToInsertIntoMainPage = 
-      insertProperty(homeHtmlToInsertIntoMainPage,
-        "randomCategoryShortName", modifiedChosenCategoryShortName);
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+     var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
+
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
